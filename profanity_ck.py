28f9e2ed04114b8ff617ef99bf02c8311b0f849a
Kalyan <46090908+kalyan-plays@users.noreply.github.com>
2022-02-24 12:57:51 +0530

    Add files via upload

--- profanity_ck.py
+++ profanity_ck.py
@@ -1,14 +1,18 @@
+##tweets.txt --> file with the list of tweets    
+with open('tweets.txt') as f:
+    words_list=[word for line in f for word in line.split()]
+##profanity_words --> file with list of profanity words       
+with open('profanity.txt') as f:
+    profanity_words=[word for line in f for word in line.split()]
+i = 0 ;
+for w in words_list:
+    if w in profanity_words:
+        i = i + 1 ;
+
+## deg of profanity is number of profine words by total words 
+
+deg_of_prof = i / len(words_list)
+
+print(deg_of_prof)
 
 
-
-
-
-
-
-
-    
-with open('new 2.txt') as f:
-    flat_list=[word for line in f for word in line.split()]
-           
-
-print(flat_list)
